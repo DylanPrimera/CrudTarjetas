bfae45fc1f8b9bb0c964c56d33f5fcb194841be1
DylanPrimera <dylan.primera@beamar.co>
2022-02-01 18:27:09 -0500

    remove apikey

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDWIFfn0XgjfKRVrZzPMZ5zIhjVrkSBNh8",
-    authDomain: "tarjetascredito-c3d53.firebaseapp.com",
-    projectId: "tarjetascredito-c3d53",
-    storageBucket: "tarjetascredito-c3d53.appspot.com",
-    messagingSenderId: "338272625788",
-    appId: "1:338272625788:web:8ce0928707d99bbfbb95f9"
+    apiKey: '<your-key>',
+    authDomain: '<your-project-authdomain>',
+    databaseURL: '<your-database-URL>',
+    projectId: '<your-project-id>',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '<your-messaging-sender-id>',
+    appId: '<your-app-id>',
   }
 };
 
